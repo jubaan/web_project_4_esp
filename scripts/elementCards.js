0e902cc5742a9a0e99e4a04650da9c2507eab6ce
Julio Añoveros <julio.ab@outlook.com>
2022-06-13 19:08:54 -0500

    feat(likeButton): Example with Horacio

---
+++ scripts/elementCards.js
@@ -0,0 +1,59 @@
+const initialCards = [
+  {
+    name: 'Valle de Yosemite',
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+  },
+  {
+    name: 'Lago Louise',
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+  },
+  {
+    name: 'Montañas Calvas',
+    link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+  },
+  {
+    name: 'Latemar',
+    link: 'https://code.s3.yandex.net/web-code/latemar.jpg',
+  },
+  {
+    name: 'Parque Nacional de la Vanoise',
+    link: 'https://code.s3.yandex.net/web-code/vanoise.jpg',
+  },
+  {
+    name: 'Lago di Braies',
+    link: 'https://code.s3.yandex.net/web-code/lago.jpg',
+  },
+];
+
+window.addEventListener('load', function () {
+  const cardElementTemplate = document.querySelector('#elementCard').content;
+  const elements = document.querySelector('.elements');
+
+  initialCards.forEach((elementDetails) => {
+    const cardElement = cardElementTemplate
+      .querySelector('.element')
+      .cloneNode(true);
+
+    cardElement.querySelector('.element__place-name').textContent =
+      elementDetails.name;
+    cardElement.querySelector('.element__place-image').src = elementDetails.link;
+    cardElement.querySelector('.element__place-image').alt =
+      elementDetails.name;
+    cardElement.querySelector('.element__form-label').for = elementDetails.name.toLowerCase().split(" ").join('-');
+    cardElement.querySelector('.form__item_type_checkbox').name =
+      elementDetails.name.toLowerCase().split(' ').join('-');
+    cardElement.querySelector('.form__item_type_checkbox').id =
+      elementDetails.name.toLowerCase().split(' ').join('-');
+    cardElement.querySelector('.form__item_type_checkbox').value =
+      elementDetails.name.toLowerCase().split(' ').join('-');
+
+    elements.append(cardElement);
+  });
+
+  const likeButton = cardElementTemplate.cardElementTemplate
+    .querySelector('.element')
+    .cloneNode(true).querySelector('.form__item');
+  likeButton.addEventListener('click', function () {
+    likeButton.checked = true;
+  });
+});
