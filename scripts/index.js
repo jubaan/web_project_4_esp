5f991b20cfed406e3b152fe64fc5f28cd7491777
Julio Añoveros <julio.ab@outlook.com>
2022-06-26 12:53:40 -0500

    refactor: move code to utils, index files and delete unnecesary files

---
+++ scripts/index.js
@@ -0,0 +1,198 @@
+import Card from './Card.js';
+import { places } from './places.js';
+
+
+// Add new Card Form
+
+const addNewCardForm = document.querySelector(
+  '.modal-form__container.add-newcard'
+);
+let imageTitle = addNewCardForm.querySelector('#imagetitle');
+let imageURL = addNewCardForm.querySelector('#imageurl');
+let elementsSection = document.querySelector('.elements');
+
+const addNewCardToArray = function (newCard) {
+  const previewImageButtons = Array.from(
+    document.querySelectorAll('.element__place-image')
+  );
+
+  previewImageButtons.push(newCard);
+};
+
+addNewCardForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (!imageTitle.validity.valid && !imageURL.validity.valid) {
+    return;
+  }
+
+  if (imageTitle.value === '' || imageURL.value === '') {
+    alert('Please enter a title and URL');
+    return;
+  }
+
+  const place = {
+    name: imageTitle.value,
+    image: imageURL.value,
+  };
+
+  const newCard = new Card(place).generateCard();
+
+  elementsSection.prepend(newCard);
+  addNewCardToArray(newCard);
+  newCard.addEventListener('click', openPreview());
+
+  addNewCardForm.classList.add('offscreen');
+  e.target.reset();
+});
+
+
+
+// Add cards to elements section
+const elements = document.querySelector('.elements');
+
+const addElement = function () {
+  places.forEach((elementDetails) => {
+    const cardElement = new Card(elementDetails);
+
+    elements.append(cardElement.generateCard());
+  });
+};
+
+addElement();
+
+
+// Edit profile form
+const modalForm = document.querySelector('.modal-form__container.edit-profile');
+let profileNameOld = modalForm.querySelector('#name');
+let profession = modalForm.querySelector('#profession');
+let profileNameNew = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+modalForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (profileNameOld.value.length > 0 && profession.value.length > 0) {
+    profileNameNew.innerText = profileNameOld.value;
+    profileProfession.innerText = profession.value;
+    modalForm.classList.add('offscreen');
+  }
+});
+
+// open containers
+
+const editProfileButton = document.querySelector('.profile__button_edit');
+
+editProfileButton.addEventListener('click', function () {
+  const editProfileModalForm = document.querySelector(
+    '.modal-form__container.edit-profile'
+  );
+
+  editProfileModalForm.classList.remove('offscreen');
+});
+
+const addNewCardButton = document.querySelector('.profile__button_add');
+
+addNewCardButton.addEventListener('click', function () {
+  const addNewCardModalForm = document.querySelector(
+    '.modal-form__container.add-newcard'
+  );
+
+  addNewCardModalForm.classList.remove('offscreen');
+});
+
+const openPreview = function () {
+  const previewImageButtons = Array.from(
+    document.querySelectorAll('.element__place-image')
+  );
+
+  previewImageButtons.map((previewImageButton) => {
+    previewImageButton.addEventListener('click', function (e) {
+      const previewImageContainer = document.querySelector(
+        '.modal-preview__container'
+      );
+
+      preview(e);
+      previewImageContainer.classList.remove('offscreen');
+    });
+  });
+};
+
+openPreview();
+
+
+//  close containers
+
+const closeButtons = Array.from(document.querySelectorAll('.container__close'));
+
+closeButtons.map((closeButton) => {
+  closeButton.addEventListener('click', function (e) {
+    e.target.parentElement.parentElement.classList.add('offscreen');
+    e.target.parentElement.reset();
+  });
+});
+
+const hideInputErr = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(
+    `.${inputElement.id}-input-error`
+  );
+
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const closeContainers = Array.from(document.querySelectorAll('.container'));
+
+closeContainers.map((closeContainer) => {
+  closeContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('container')) {
+      closeContainer.classList.add('offscreen');
+    }
+    const form = closeContainer.querySelector('.form');
+    const inputs = Array.from(form.querySelectorAll('.form__input'));
+
+    inputs.map((input) => {
+      hideInputErr(form, input);
+    });
+  });
+
+  window.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      closeContainer.classList.add('offscreen');
+    }
+  });
+});
+
+class Form {
+  constructor(formSelectors) {
+    this._openButton = formSelectors.openButton;
+    this._closeButton = formSelectors.closeButton;
+  }
+
+  _getTemplate() {}
+
+  generateForm() {}
+
+  _setEventLlisteners() {}
+
+  _openForm() {}
+
+  _closeForm() {}
+
+  _handleSubmit() {}
+
+  _resetForm() {}
+}
+
+class profileForm extends Form {
+  constructor() {
+    super();
+  }
+}
+
+class placeForm extends Form {
+  constructor() {
+    super();
+  }
+}
